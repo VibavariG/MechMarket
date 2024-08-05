cd96ac8cd505442dc777ba9c57c1b308170bdde9
VibavariG <vibavari2301@gmail.com>
2024-08-05 08:17:22 -0400

    BFE-36: update detail view, use shortcut for http404

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i9e^p#t999x&xibn@6fzd1o6!a0$guobo3wjmy34m-bi&z^^%f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
