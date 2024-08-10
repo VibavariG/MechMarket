a99d0f6236f46abf3a8dd4d40bcd1285f3b9d5ca
VibavariG <vibavari2301@gmail.com>
2024-08-09 22:18:47 -0400

    Fix datetime error

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-from datetime import datetime
+from datetime import timedelta
 
 # Create your models here.
 class Question(models.Model):
@@ -10,7 +10,7 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        return self.pub_date >= timezone.now() + timedelta(days=1)
 
 
 class Choice(models.Model):
