be42a4483cbcc357abd61da67597d2033ba0c3a0
VibavariG <vibavari2301@gmail.com>
2024-08-11 21:46:40 -0400

    Add business index page

--- mysite/business/urls.py
+++ mysite/business/urls.py
@@ -5,9 +5,10 @@ from . import views
 app_name = "business"
 urlpatterns = [
     # ex: /business/
-    path("", views.IndexView.as_view(), name="index"),
+    path("", views.index, name="index")
+    # path("", views.IndexView.as_view(), name="index"),
     # ex: /business/5/
-    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    # path("<int:pk>/", views.DetailView.as_view(), name="detail"),
     # # ex: /business/5/results/
     # path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     # # ex: /polls/5/vote/
--- mysite/business/views.py
+++ mysite/business/views.py
@@ -1,6 +1,6 @@
 from django.db.models import F
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.views import generic
 from .models import Product
@@ -9,10 +9,13 @@ from django.utils import timezone
 
 # Create your views here.
 
-class IndexView(generic.ListView):
-    template_name = "business/index.html"
-    context_object_name = "latest_question_list"
+def index(request):
+    return HttpResponse("Hi, it's the index page")
+# class IndexView(generic.ListView):
+#     template_name = "business/index.html"
+#     return H
+#     context_object_name = "latest_question_list"
 
-    def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.filter(choice__isnull=False).distinct().filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
+#     def get_queryset(self):
+#         """Return the last five published questions."""
+#         return Question.objects.filter(choice__isnull=False).distinct().filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
